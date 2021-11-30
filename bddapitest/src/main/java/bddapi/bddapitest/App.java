9b57e5974a756f291b7cfd860a490991a3d5273b
sivaraj <sivaraj@192.168.0.9>
2021-12-01 01:50:52 +0530

    bdd commit

---
+++ bddapitest/src/main/java/bddapi/bddapitest/App.java
@@ -0,0 +1,13 @@
+package bddapi.bddapitest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
